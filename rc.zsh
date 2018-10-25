dc2b8eb13b1fdc83961179b9adf243f6b3584c0b
Jesús Germade <jesus@germade.es>
2018-10-25 15:52:11 +0200

    Added if esists /home/joshua/bin and /home/joshua/.local/bin to /home/joshua/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin

--- rc.zsh
+++ rc.zsh
@@ -38,3 +38,14 @@ function precmd {
     print -Pn "\e]0;$(pwd | sed -e "s,^$HOME,~,")$(git_current_branch)\a"
   fi
 }
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
