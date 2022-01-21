700c5d429dad2bd65f0c5da7c80c57ed3ad4c1f9
Jesús Germade <jesus@germade.es>
2022-01-21 17:20:22 +0100

    fix for server.zsh

--- server.zsh
+++ server.zsh
@@ -1,5 +1,5 @@
 
-source "$(dirname "$0")"/rc.sh
+source "$(dirname "$0")"/rc.zsh
 
 local ret_status="%(?:%{$fg_bold[white]%}➤ :%{$fg_bold[red]%}➤ %s)"
 PROMPT='%{$fg[blue]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%} %{$fg_bold[green]%}%p %{$fg[yellow]%}%~ %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}${ret_status}% %{$reset_color%}'
